b7f12ec3b2637f5105b1c7658cb79775ee157ee1
Unknown <avilesgomezjonathan@hotmail.com>
2018-02-03 19:00:24 -0600

    Tienda2.0

---
+++ App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import Login from './android/Componentes/Login'
+import Index from './android/Componentes/Cuerpo/Index'
+import subirAnuncio from './android/Componentes/Cuerpo/subirAnuncio'
+import MisAnuncios from './android/Componentes/Cuerpo/MisAnuncios'
+
+const RootNavigator = StackNavigator({
+    Login :{screen:Login},
+    Index : {screen: Index,
+      navigationOptions:{
+        header: null,
+    }},
+    subirAnuncio : {screen: subirAnuncio},
+    MisAnuncios : {screen: MisAnuncios},
+
+});
+
+export default RootNavigator;
